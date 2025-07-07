c32d4977f8a3a73f15effd82ec0c6bc92a6419b3
pratik352 <pratiksomwanshi30@outlook.com>
2025-07-07 18:06:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,7 @@ variable "vm_count" {}
 variable "vm_name_prefix" {}
 variable "admin_username" {}
 variable "admin_password" {}
+variable "azure_credentials" {
+  description = "Azure service principal credentials in JSON format"
+  type        = string
+}
